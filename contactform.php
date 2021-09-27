03dd658b75e4bdbda22a49595d54909d88d512d9
Ben West <benwest0291@outlook.com>
2021-09-27 21:34:41 +0100

    mobile ready

---
+++ contactform.php
@@ -0,0 +1,21 @@
+<?php
+ $name = $_post['name'];
+ $visitor_email = $_POST['email'];
+ $message = $_POST['message'];
+
+ $email_form = 'benwest0291@outlook.com';
+
+ $email_subject = "New Form Submission";
+
+ $email_body "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+
+
+                $to = "bwest0291@gmail.com";
+
+                $headers = "Form: $email_from \r\n";
+                $headers = "Reply-To: $vistitor_email \r\n";
+                mail($to,$email_subject,$email_body,$headers);
+                header("Location: index.html");
+?>
